c7aafbf537a0c430c587908cf40afb1aaf6097e6
dorumamu <64189518+dorumamu@users.noreply.github.com>
2020-09-14 15:49:43 +0900

    원래 만들었던거 지움 회원가입창 sign up 페이지만듬

--- app/src/main/java/com/cookandroid/ccit_suda/sign_up.java
+++ app/src/main/java/com/cookandroid/ccit_suda/sign_up.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ChildActivity extends AppCompatActivity {
+public class sign_up extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_child);
+        setContentView(R.layout.activity_sign_up);
     }
 }
