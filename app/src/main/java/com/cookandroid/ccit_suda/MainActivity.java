e5cd808f8f3032376aeaf29ab005644bb95ec716
seungji <kwag57@naver.com>
2020-09-14 17:36:25 +0900

    test

--- app/src/main/java/com/cookandroid/ccit_suda/MainActivity.java
+++ app/src/main/java/com/cookandroid/ccit_suda/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.button12);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), sign_up.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
