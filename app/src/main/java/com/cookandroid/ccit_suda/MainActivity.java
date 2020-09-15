0247e5a0f09c98ba0b27da5dd7eefade0f2867e0
seungji <kwag57@naver.com>
2020-09-15 18:53:51 +0900

    network permission xml add

--- app/src/main/java/com/cookandroid/ccit_suda/MainActivity.java
+++ app/src/main/java/com/cookandroid/ccit_suda/MainActivity.java
@@ -2,26 +2,51 @@ package com.cookandroid.ccit_suda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.os.AsyncTask;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn = (Button) findViewById(R.id.button12);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+
+
+//        Button btn1 = (Button) findViewById(R.id.Register);
+        Button btn2 = (Button) findViewById(R.id.Login);
+//
+//
+//        btn1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getApplicationContext(), sign_up.class);
+//                startActivity(intent);
+//            }
+//        });
+        btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), sign_up.class);
-                startActivity(intent);
+
             }
         });
+
+
+
     }
+
 
 }
