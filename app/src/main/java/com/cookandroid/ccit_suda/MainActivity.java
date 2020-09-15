c7f20ce264243e7eff003960ba81316c5ad1a60d
seungji <kwag57@naver.com>
2020-09-16 03:31:53 +0900

    http 통신 구현 로그인 성공시 다음창 띄우기

--- app/src/main/java/com/cookandroid/ccit_suda/MainActivity.java
+++ app/src/main/java/com/cookandroid/ccit_suda/MainActivity.java
@@ -2,28 +2,31 @@ package com.cookandroid.ccit_suda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ContentValues;
-import android.os.AsyncTask;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
 
 //        Button btn1 = (Button) findViewById(R.id.Register);
@@ -41,12 +44,73 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+//                Toast.makeText(getApplicationContext(),value+'\n'+value1,Toast.LENGTH_SHORT).show();
+                sendRequest();
             }
         });
 
 
+        if (AppHelper.requestQueue == null) {
+            AppHelper.requestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
 
     }
 
+    public void sendRequest() {
+        String url = "http://10.0.2.2/login";
+        StringRequest request = new StringRequest(
+                Request.Method.POST,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getApplicationContext(), "응답->" + response, Toast.LENGTH_SHORT).show();
+//                        Log.v("TAG",response.equals("1"));
+                        if(response.equals("1")){
+                            Intent intent = new Intent(getApplicationContext(), boardActivity.class);
+                            startActivity(intent);
+                            Toast.makeText(getApplicationContext(), "로그인 성공", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(), "로그인 실패", Toast.LENGTH_SHORT).show();
+                        }
+
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "에러 ->" + error.getMessage(), Toast.LENGTH_SHORT).show();
+                        Log.v("TAG", error.toString());
+                    }
+                }
+
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                EditText userid = (EditText) findViewById(R.id.ID);
+                String value = userid.getText().toString();
+                EditText userpw = (EditText) findViewById(R.id.PW);
+                String value1 = userpw.getText().toString();
+                params.put("id", value);
+                params.put("pw", value1);
+                return params;
+            }
+
+//            public Map<String, String> getHeader() throws AuthFailureError{
+//                Map<String, String> params = new HashMap<String, String >();
+//                params.put("Content-Type", "application/x-www-form-urlencoded");
+//                return params;
+//            }
+        };
+        request.setShouldCache(false);
+
+//        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        AppHelper.requestQueue.add(request);
+        Toast.makeText(getApplicationContext(), "요청 보냄", Toast.LENGTH_SHORT).show();
+    }
 
 }
