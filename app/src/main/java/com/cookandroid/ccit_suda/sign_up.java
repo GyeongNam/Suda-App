f11c0df20d96b1c12f7e4ab2e872e5c47ae55045
dorumamu <64189518+dorumamu@users.noreply.github.com>
2020-09-15 19:20:45 +0900

    회원가입페이지 수정

--- app/src/main/java/com/cookandroid/ccit_suda/sign_up.java
+++ app/src/main/java/com/cookandroid/ccit_suda/sign_up.java
@@ -1,7 +1,11 @@
 package com.cookandroid.ccit_suda;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class sign_up extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class sign_up extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Button Btn1 = (Button) findViewById(R.id.button3);
+
+        Btn1.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
