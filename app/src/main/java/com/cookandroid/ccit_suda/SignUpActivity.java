823035be5944fbccb3c7d8d78c34c711e7d96840
dorumamu <64189518+dorumamu@users.noreply.github.com>
2020-09-17 13:24:27 +0900

    회원가입레이아웃재생성

--- app/src/main/java/com/cookandroid/ccit_suda/SignUpActivity.java
+++ app/src/main/java/com/cookandroid/ccit_suda/SignUpActivity.java
@@ -2,6 +2,7 @@ package com.cookandroid.ccit_suda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SignUpActivity extends AppCompatActivity {
@@ -10,5 +11,6 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
     }
 }
