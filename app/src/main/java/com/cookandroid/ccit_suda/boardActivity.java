d53cb8c7167e2f2068a269dc4851851b7f12b76c
seungji <kwag57@naver.com>
2020-09-17 19:08:09 +0900

    메인화면 만드는중

--- app/src/main/java/com/cookandroid/ccit_suda/boardActivity.java
+++ app/src/main/java/com/cookandroid/ccit_suda/boardActivity.java
@@ -1,17 +1,69 @@
 package com.cookandroid.ccit_suda;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class boardActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+    private View drawerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
+
+        drawerLayout = (DrawerLayout)findViewById(R.id.activity_board);
+        drawerView =(View)findViewById(R.id.drawer);
+
+        ImageButton btn_open = (ImageButton) findViewById(R.id.btn_open);
+        btn_open.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                drawerLayout.openDrawer(drawerView);
+            }
+        });
+        drawerLayout.setDrawerListener(listener);
+        drawerView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return true;
+            }
+        });
+        Button btn_close = (Button)findViewById(R.id.btn_close);
+        btn_close.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.closeDrawers();
+            }
+        }));
+
     }
+    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
+        @Override
+        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+
+        }
+
+        @Override
+        public void onDrawerOpened(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerClosed(@NonNull View drawerView) {
+
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState) {
+
+        }
+    };
 }
