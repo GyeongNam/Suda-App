fb3f1826540561bdff0e1a93ba1befbd653f153f
seungji <kwag57@naver.com>
2020-09-24 01:32:23 +0900

    게시글 상세보기 틀

--- app/src/main/java/com/cookandroid/ccit_suda/PostdetailActivity.java
+++ app/src/main/java/com/cookandroid/ccit_suda/PostdetailActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,10 +27,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PostdetailActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_postdetail);
+
 
         sendRequest();
     }
@@ -44,9 +47,20 @@ public class PostdetailActivity extends AppCompatActivity {
                         SharedPreferences sharedPreferences = getSharedPreferences("File", 0);
                         String userinfo = sharedPreferences.getString("userinfo", "");
                         TextView username = (TextView) findViewById(R.id.username);
+                        TextView title = (TextView) findViewById(R.id.Title);
+                        TextView text = (TextView) findViewById(R.id.Text);
                         username.setText("환영합니다 " + userinfo + " 님");
                         Toast.makeText(getApplicationContext(), "응답->" + response, Toast.LENGTH_SHORT).show();
                         Log.v("TAG", response);
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            Log.v("TAG","게시글 디테일"+jsonObject.getString("Title"));
+                            title.setText(jsonObject.getString("Title"));
+                            text.setText(jsonObject.getString("Text"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
                     }
                 },
                 new Response.ErrorListener() {
