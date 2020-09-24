134be7ce1ff5b0102295f26cdf4844f7b3a23a0f
seungji <kwag57@naver.com>
2020-09-25 03:08:47 +0900

    댓글 리스트뷰 변경

--- app/src/main/java/com/cookandroid/ccit_suda/PostdetailActivity.java
+++ app/src/main/java/com/cookandroid/ccit_suda/PostdetailActivity.java
@@ -7,16 +7,22 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,30 +33,39 @@ import com.android.volley.error.VolleyError;
 import com.android.volley.request.MultiPartRequest;
 import com.android.volley.request.SimpleMultiPartRequest;
 import com.android.volley.request.StringRequest;
+import com.google.gson.JsonArray;
 
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class PostdetailActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private View drawerView;
+    private LinearLayout container;
+    private ListView postlist;
+     List<String> replylist = new ArrayList<>() ;
+     private ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_postdetail);
+
         drawerLayout = (DrawerLayout) findViewById(R.id.activity_postdetail);
         drawerView = (View) findViewById(R.id.drawer);
-
-
-
-
-
+        container = (LinearLayout) findViewById(R.id.parentlayout);
+        postlist = (ListView) findViewById(R.id.postlist);
+//        ArrayList replylist = new ArrayList<String>();
+        postlist.setVerticalScrollBarEnabled(false);
+//        adapter.notifyDataSetChanged();
         ImageButton btn_open = (ImageButton) findViewById(R.id.btn_open);
         btn_open.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -96,6 +111,9 @@ public class PostdetailActivity extends AppCompatActivity {
 
 
         sendRequest();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, replylist);
+        postlist.setAdapter(adapter);
+
     }
     DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
         @Override
@@ -119,32 +137,52 @@ public class PostdetailActivity extends AppCompatActivity {
         }
     };
     public void sendRequest() {
-        String url = "http://10.0.2.2/test22"; //"http://ccit2020.cafe24.com:8082/login";
+        String url = "http://10.0.2.2/post_detail"; //"http://ccit2020.cafe24.com:8082/login";
         StringRequest request = new StringRequest (
                 Request.Method.POST,
                 url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
+
                         SharedPreferences sharedPreferences = getSharedPreferences("File", 0);
                         String userinfo = sharedPreferences.getString("userinfo", "");
                         TextView username = (TextView) findViewById(R.id.username);
                         TextView title = (TextView) findViewById(R.id.Title);
                         TextView text = (TextView) findViewById(R.id.Text);
-                        TextView reply = (TextView) findViewById(R.id.reply);
+                        HashMap<Integer,String> map = new HashMap<>();
                         username.setText("환영합니다 " + userinfo + " 님");
                         Toast.makeText(getApplicationContext(), "응답->" + response, Toast.LENGTH_SHORT).show();
                         Log.v("TAG", response);
+
                         try {
-                            JSONObject jsonObject = new JSONObject(response);
-                            Log.v("TAG","게시글 디테일"+jsonObject.getString("Title"));
-                            reply.setText(jsonObject.getString("c_writer")+":"+jsonObject.getString("comment"));
-                            title.setText(jsonObject.getString("Title"));
-                            text.setText(jsonObject.getString("Text"));
+                            JSONArray jsonArray = new JSONArray(response);
+                            for(int i=0; i<jsonArray.length();i++){
+                                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                                Log.v("TAG","게시글 디테일"+jsonObject.getString("Title"));
+//                                reply.setText(jsonObject.getString("c_writer")+":"+jsonObject.getString("comment"));
+                                if(!replylist.contains(jsonObject.getString("c_writer")+":"+jsonObject.getString("comment"))) {
+
+                                    replylist.add(jsonObject.getString("c_writer")+":"+jsonObject.getString("comment"));
+
+                                }
+//                                replylist.add(jsonObject.getString("c_writer")+":"+jsonObject.getString("comment"));
+                                title.setText(jsonObject.getString("Title"));
+                                text.setText(jsonObject.getString("Text"));
+                            }
+                            Log.v("TAG", String.valueOf(replylist));
+//                            runOnUiThread(new Runnable() {
+//                                @Override
+//                                public void run() {
+//                                    // 갱신된 데이터 내역을 어댑터에 알려줌
+//                                    adapter.notifyDataSetChanged();
+//                                }
+//                            });
+
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
-
+                        adapter.notifyDataSetChanged();
                     }
                 },
                 new Response.ErrorListener() {
@@ -221,6 +259,7 @@ public class PostdetailActivity extends AppCompatActivity {
                         TextView username = (TextView) findViewById(R.id.username);
                         Toast.makeText(getApplicationContext(), "응답->" + response, Toast.LENGTH_SHORT).show();
                         Log.v("TAG", response);
+                        sendRequest();
                     }
                 },
                 new Response.ErrorListener() {
@@ -261,5 +300,26 @@ public class PostdetailActivity extends AppCompatActivity {
     }
     public void sendreply(View view){
         sendreply();
+    }
+    //텍스트뷰 동적생성하기
+    public void textview(final String a, android.widget.LinearLayout container){
+        //TextView 생성
+        final TextView view1 = new TextView(this);
+        view1.setText(a);
+        view1.setTextSize(20);
+        view1.setTextColor(Color.BLACK);
+        Drawable drawable = getResources().getDrawable(R.drawable.round_border);
+        view1.setBackground(drawable);
+        //layout_width, layout_height, gravity 설정
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(30,30,10,30);
+
+
+        view1.setLayoutParams(lp);
+
+
+
+        //부모 뷰에 추가
+        container.addView(view1);
     }
 }
