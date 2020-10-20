833ecfb9841551b3598f64b8b377c4ded8f489d0
GyeongNam <knightanne97@naver.com>
2020-10-20 10:30:35 +0900

    log: re2

--- app/src/main/java/com/cookandroid/ccit_suda/MainActivity.java
+++ app/src/main/java/com/cookandroid/ccit_suda/MainActivity.java
@@ -69,8 +69,10 @@ public class MainActivity extends AppCompatActivity {
             startService(new Intent(this, ForcedTerminationService.class));
             String android = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
 
-
-            pushlog();
+            if(a.a == 0){
+                pushlog();
+                a.a=1;
+            }
 
             File file = new File("/mnt/sdcard/log.file");
             file.delete();
@@ -120,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void pushlog() {                                         // 로그파일 전송
         String android = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
-        String url = "http://ccit2020.cafe24.com:8082/get_logfile"; //10.0.2.2 ccit2020.cafe24.com:8082
+        String url = "http://10.0.2.2/get_logfile"; //10.0.2.2 ccit2020.cafe24.com:8082
         SimpleMultiPartRequest smpr= new SimpleMultiPartRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
