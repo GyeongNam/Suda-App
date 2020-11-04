8fd6ea6e0c285f1b937af528f95ef224a7a5125a
dorumamu <64189518+dorumamu@users.noreply.github.com>
2020-11-04 10:37:54 +0900

    채팅 서버

--- app/src/main/java/com/cookandroid/ccit_suda/chatting.java
+++ app/src/main/java/com/cookandroid/ccit_suda/chatting.java
@@ -15,43 +15,50 @@ import java.net.URISyntaxException;
 
 import io.socket.client.Socket;
 import io.socket.client.IO;
+import io.socket.emitter.Emitter;
 
 import static io.socket.client.IO.socket;
 
 
 public class chatting extends AppCompatActivity {
-    String Tag = "chatting";
-    static Socket mSocket;
+//    String Tag = "chatting";
+//    static Socket mSocket;
+private String TAG = "MainActivity";
+    private Socket mSocket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatting);
 
-        //IO.socket 메소드는 은 저 URL 을 토대로 클라이언트 객체를 Return 합니다.
         try {
             mSocket = IO.socket("http://10.0.2.2/chartEvent");
-            mSocket.on(Socket.EVENT_CONNECT, (Object... objects) -> {
-            JsonObject preJsonObject = new JsonObject();
-            preJsonObject.addProperty("roomName", "myroom");
-            JSONObject jsonObject = null;
+            mSocket.connect();
+            mSocket.on(Socket.EVENT_CONNECT, onConnect);
+            mSocket.on("chartEvent", onMessageReceived);
+        } catch(URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+    private Emitter.Listener onConnect = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            mSocket.emit("chartEvent", "ccit");
+        }
+    };
+
+    // 서버로부터 전달받은 'chat-message' Event 처리.
+    private Emitter.Listener onMessageReceived = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            // 전달받은 데이터는 아래와 같이 추출할 수 있습니다.
             try {
-                jsonObject = new JSONObject(preJsonObject.toString());
+                JSONObject receivedData = (JSONObject) args[0];
+                Log.d(TAG, receivedData.getString("msg"));
+                Log.d(TAG, receivedData.getString("data"));
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-            mSocket.emit("joinRoom",jsonObject);
-        }).on("recMsg", (Object... objects) -> {
-            JsonParser jsonParsers = new JsonParser();
-            JsonObject jsonObject = (JsonObject) jsonParsers.parse(objects[0] + "");
-            runOnUiThread(()->{
-                Log.v("comment",jsonObject.get("comment").getAsString());
-            });
-        });
-        mSocket.connect();
-    } catch(Exception e) {
-        Log.e("mSocket",e.toString() );
-    }
-
-    }
+        }
+    };
 }
--- app/src/main/java/com/cookandroid/ccit_suda/chattingList.java
+++ app/src/main/java/com/cookandroid/ccit_suda/chattingList.java
@@ -2,13 +2,27 @@ package com.cookandroid.ccit_suda;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class chattingList extends AppCompatActivity {
 
+    Button inbt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatting_list);
+         inbt = (Button) findViewById(R.id.inbt);
+        inbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), chatting.class);
+                startActivity(intent);
+            }
+        }
+        );
+
     }
 }
