29c31728d9326585f7020e13b36a3e0af4fa5e70
seungji <kwag57@naver.com>
2020-12-01 10:32:58 +0900

    진웅

--- app/src/main/java/com/cookandroid/ccit_suda/room/talk.java
+++ app/src/main/java/com/cookandroid/ccit_suda/room/talk.java
@@ -1,5 +1,6 @@
 package com.cookandroid.ccit_suda.room;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
@@ -8,14 +9,54 @@ import java.util.Date;
 
 @Entity(tableName = "talk_Contents")
 public class talk implements Serializable {
-    @PrimaryKey(autoGenerate = true)
-    private int idx;
-    private String user;
-    private String chatlist;
-    private int chat_room;
-    private Date date;
+    public int getIdx() {
+        return idx;
+    }
 
-    public talk(int idx, String user, String chatlist, int chat_room, Date date) {
+    public void setIdx(int idx) {
+        this.idx = idx;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getChatlist() {
+        return chatlist;
+    }
+
+    public void setChatlist(String chatlist) {
+        this.chatlist = chatlist;
+    }
+
+    public int getChat_room() {
+        return chat_room;
+    }
+
+    public void setChat_room(int chat_room) {
+        this.chat_room = chat_room;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "idx") private int idx;
+    @ColumnInfo(name = "user") private String user;
+    @ColumnInfo(name = "chatlist")private String chatlist;
+    @ColumnInfo(name = "chat_room")private int chat_room;
+    @ColumnInfo(name = "date") private String date;
+
+    public talk(int idx, String user, String chatlist, int chat_room, String date) {
         this.idx = idx;
         this.user = user;
         this.chatlist = chatlist;
