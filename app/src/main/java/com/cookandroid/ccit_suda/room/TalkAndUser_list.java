8e397c6db9b5246e2b081df4dce5dab8a50dba16
seungji <kwag57@naver.com>
2020-12-02 12:07:09 +0900

    d

---
+++ app/src/main/java/com/cookandroid/ccit_suda/room/TalkAndUser_list.java
@@ -0,0 +1,12 @@
+package com.cookandroid.ccit_suda.room;
+
+import androidx.room.Embedded;
+import androidx.room.Relation;
+
+public class TalkAndUser_list {
+    @Embedded
+    public User_list user_list;
+    @Relation(parentColumn = "room", entityColumn ="chat_room")
+    public Talk talk;
+
+}
